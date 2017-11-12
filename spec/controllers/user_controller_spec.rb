5a689a448b7481581359424e25e68ed92303829b
Tom Freudenheim <tomfreudenheim@me.com>
2017-11-12 10:00:11 -0500

    Fix UserController tests

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -5,11 +5,11 @@ require "clearance/rspec"
 RSpec.describe UsersController, type: :controller do
 
   describe "POST #create" do
-    it "returns success" do
+    it "redirects when successful" do
       user_attributes = attributes_for(:user)
 
       post :create, params: {user: user_attributes}
-      expect(response).to have_http_status(:success)
+      expect(response).to redirect_to(root_path)
       
 
     end      
@@ -17,7 +17,7 @@ RSpec.describe UsersController, type: :controller do
     it "sends a welcome email" do
       user_attributes = attributes_for(:user)
       
-      expect(post :create, params: {user: user_attributes})
+      expect { post :create, params: {user: user_attributes} }
         .to change { ActionMailer::Base.deliveries.count }.by(1)
     end
   end
