d14d9408c48887d187b9e9c87ef75c1c31314c96
Tom Freudenheim <tomfreudenheim@me.com>
2017-11-12 10:02:05 -0500

    Rename homepage controller to  ShopController THis will allow the products controller#index to return JSON list of products

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
-  root to: 'products#index'
+  root to: 'shop#index'
 end
