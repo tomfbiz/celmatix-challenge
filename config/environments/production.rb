c03fc9101ca6320a63ae1c4d1166d07882058ff4
Tom Freudenheim <tomfreudenheim@me.com>
2017-11-14 08:44:06 -0500

    Tweaks for Heroku Trying removeing deliver_now for email.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,10 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # For Heroku
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  
 end
