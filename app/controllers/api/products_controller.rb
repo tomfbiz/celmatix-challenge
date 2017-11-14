f96d03939ec0b6ef2775674990116e6e8c523e45
Tom Freudenheim <tomfreudenheim@me.com>
2017-11-14 17:10:40 -0500

    Rubocop tweaks

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,7 +1,9 @@
-class API::ProductsController < ApplicationController
-  def index
-    @products = Product.all
-    # render json: Product.all
-    render "api/products/index", format: :json
+module API
+  class ProductsController < ApplicationController
+    def index
+      @products = Product.all
+      # render json: Product.all
+      render "api/products/index", format: :json
+    end
   end
 end
