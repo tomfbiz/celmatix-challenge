d3b934c6b399f3a253419f055b9aab291f4316ee
Tom Freudenheim <tomfreudenheim@me.com>
2017-11-16 12:25:41 -0500

    FIx email to be vaild

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   include Clearance::User
   has_one :cart
 
-  DEFAULT_ADMIN_EMAIL = "admin@celmatix_challenge.com".freeze
+  DEFAULT_ADMIN_EMAIL = "admin@celmatixchallenge.com".freeze
 
   def admin?
     email == DEFAULT_ADMIN_EMAIL || admin
